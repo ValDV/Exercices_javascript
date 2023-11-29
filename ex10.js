55aa34cda0a1c0088696c39e7fd23327138ee63d
ValDV <valentinrsr20@gmail.com>
2023-11-29 08:37:12 +0100

    exo 10 fini

--- ex10.js
+++ ex10.js
@@ -11,17 +11,17 @@ var MaDiv = document.getElementById("Methode4");
 MaDiv.addEventListener("keydown", UneProcedureQuiEcrit);
 
 function UneProcedureQuiEcrit(evenement) {
-    evenement.target.innerHTML = "on a appuyé sur : "+evenement.key;
+    evenement.target.innerHTML = "On a appuyé sur : "+evenement.key;
 }
 
 function UneProcedureQuiChangeLeText(evenement) {
     evenement.target.className = 'divChanged';
     compteur++;
-    evenement.target.innerHTML = "on m'a cliqué dessus : "+compteur+ " fois";
+    evenement.target.innerHTML = "On m'a cliqué dessus : "+compteur+ " fois";
 }
 
 function UneProcedureQuiSurvole(evenement) {
     evenement.target.className = 'survol';
     compteur2++;
-    evenement.target.innerHTML = "on m'a survolé : "+compteur2+ " fois";
+    evenement.target.innerHTML = "On m'a survolé : "+compteur2+ " fois";
 }
